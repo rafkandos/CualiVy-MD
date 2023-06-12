e3e99df304c66f85fbb9c3d26f08208f8e5b8947
erniseptia <92901309+erniseptia@users.noreply.github.com>
2023-06-12 14:03:09 +0700

    Fiksasi ListJob, Detail, Apply

--- app/src/main/java/app/project/cualivy_capstone/adapter/JobAdapter.kt
+++ app/src/main/java/app/project/cualivy_capstone/adapter/JobAdapter.kt
@@ -12,11 +12,15 @@ import app.project.cualivy_capstone.detail.DetailActivity
 import app.project.cualivy_capstone.preference.PreferenceManager
 import app.project.cualivy_capstone.response.Detail
 import app.project.cualivy_capstone.response.JobData
+import com.bumptech.glide.Glide
+import org.json.JSONObject
 
 
-class JobAdapter(private val listJob: ArrayList<String>) : RecyclerView.Adapter<JobAdapter.ViewHolder>() {
 
-    private var onItemClickListener: ((String) -> Unit)? = null
+class JobAdapter(private val listJob: ArrayList<JSONObject>) :
+    RecyclerView.Adapter<JobAdapter.ViewHolder>() {
+
+    private var onItemClickListener: ((JSONObject) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_job, parent, false)
@@ -27,20 +31,13 @@ class JobAdapter(private val listJob: ArrayList<String>) : RecyclerView.Adapter<
         val job = listJob[position]
         holder.bind(job)
 
-//        holder.itemView.setOnClickListener {
-//            onItemClickListener?.invoke(job)
-//        }
-
         holder.itemView.setOnClickListener {
-            val intent = Intent(holder.itemView.context, DetailActivity::class.java)
-//            intent.putExtra(DetailActivity.EXTRA_NAME, "dicoding.com")
-////            val jobId = position + 1
-////            val url = "https://www.dicoding.com/$jobId"
-//            val url = "https://www.dicoding.com/"
-//            intent.putExtra(DetailActivity.EXTRA_URL, "")
-            PreferenceManager.saveGuid(JobData(job.substring(0, 36), ""))
-            holder.itemView.context.startActivity(intent)
-
+//            onItemClickListener?.invoke(job)
+       val intent = Intent(holder.itemView.context, DetailActivity::class.java)
+//          PreferenceManager.saveGuid(JobData(job.substring(0, 36), ""))
+        val jobData = JobData(guid = job.getString("guid"), link = "")
+          PreferenceManager.saveGuid(jobData)
+          holder.itemView.context.startActivity(intent)
         }
     }
 
@@ -48,17 +45,93 @@ class JobAdapter(private val listJob: ArrayList<String>) : RecyclerView.Adapter<
         return listJob.size
     }
 
-    fun setOnItemClickListener(listener: (String) -> Unit) {
+    fun setOnItemClickListener(listener: (JSONObject) -> Unit) {
         onItemClickListener = listener
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val tvItem: TextView = view.findViewById(R.id.tvItem)
+        private val tvCompany: TextView = view.findViewById(R.id.tv_company)
+        private val tvLocation: TextView = view.findViewById(R.id.tv_location)
+        private val imgUser: ImageView = view.findViewById(R.id.img_user)
 
-        fun bind(job: String) {
-            tvItem.text = job
+        fun bind(job: JSONObject) {
+            val position = job.getString("position")
+            val company = job.getString("companyname")
+            val location = job.getString("location")
+            val image = job.getString("image")
 
+            tvItem.text = position
+            tvCompany.text = company
+            tvLocation.text = location
 
+            Glide.with(itemView)
+                .load(image)
+                .into(imgUser)
         }
     }
 }
+
+//
+//class JobAdapter(private val listJob: ArrayList<JSONObject>) : RecyclerView.Adapter<JobAdapter.ViewHolder>() {
+//
+//    private var onItemClickListener: ((JSONObject) -> Unit)? = null
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_job, parent, false)
+//        return ViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        val job = listJob[position]
+//        holder.bind(job)
+//
+////        holder.itemView.setOnClickListener {
+////            onItemClickListener?.invoke(job)
+////        }
+//
+//        holder.itemView.setOnClickListener {
+//            val intent = Intent(holder.itemView.context, DetailActivity::class.java)
+////            PreferenceManager.saveGuid(JobData(job.substring(0, 36), ""))
+//            val jobData = JobData(guid = job.getString("guid"), link = "")
+//            PreferenceManager.saveGuid(jobData)
+//            holder.itemView.context.startActivity(intent)
+//
+//        }
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return listJob.size
+//    }
+//
+//    fun setOnItemClickListener(listener: (JSONObject) -> Unit) {
+//        onItemClickListener = listener
+//    }
+//
+//    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+////        private val tvItem: TextView = view.findViewById(R.id.tvItem)
+//        private val tvPosition: TextView = view.findViewById(R.id.tv_position)
+//        private val tvCompany: TextView = view.findViewById(R.id.tv_company)
+//        private val tvLocation: TextView = view.findViewById(R.id.tv_location)
+//        private val imgUser: ImageView = view.findViewById(R.id.img_user)
+//
+//
+//        fun bind(job: JSONObject) {
+//            val position = job.getString("position")
+//            val company = job.getString("companyname")
+//            val location = job.getString("location")
+//            val image = job.getString("image")
+//
+//            tvPosition.text = position
+//            tvCompany.text = company
+//            tvLocation.text = location
+//
+//            Glide.with(itemView)
+//                .load(image)
+//                .into(imgUser)
+//
+//
+//
+//        }
+//    }
+//}
--- app/src/main/java/app/project/cualivy_capstone/recyclerview/ListJobViewModel.kt
+++ app/src/main/java/app/project/cualivy_capstone/recyclerview/ListJobViewModel.kt
@@ -1,14 +1,11 @@
 package app.project.cualivy_capstone.recyclerview
 
-import android.content.ContentValues.TAG
-import android.content.Context
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import app.project.cualivy_capstone.api.ApiConfig
 import app.project.cualivy_capstone.preference.PreferenceManager
-import app.project.cualivy_capstone.response.Detail
 import com.bumptech.glide.Glide
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
@@ -18,19 +15,14 @@ import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 import java.util.*
-import kotlin.collections.ArrayList
 
 class ListJobViewModel : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> get() = _isLoading
 
-    private val _listJob = MutableLiveData<ArrayList<String>>()
-    val listJob: LiveData<ArrayList<String>> get() = _listJob
-//    private val _listJob = MutableLiveData<JSONArray>()
-//    val listJob: LiveData<JSONArray> get() = _listJob
-
-
+    private val _listJob = MutableLiveData<ArrayList<JSONObject>>()
+    val listJob: LiveData<ArrayList<JSONObject>> get() = _listJob
 
     private val _toastMessage = MutableLiveData<String>()
     val toastMessage: LiveData<String> get() = _toastMessage
@@ -39,7 +31,6 @@ class ListJobViewModel : ViewModel() {
         _isLoading.value = true
 
         val client = AsyncHttpClient()
-//        val url = ApiConfig.getApiService().searchJob(position, company, location, notes, thirdparty)
         val url = "http://34.124.223.74/api/Job/SearchJob"
         val params = RequestParams()
         params.put("input", base64Image)
@@ -53,28 +44,14 @@ class ListJobViewModel : ViewModel() {
             ) {
                 _isLoading.value = false
 
-                val listJob = ArrayList<String>()
-                val listGuid = ArrayList<String>()
+                val listJob = ArrayList<JSONObject>()
                 val result = String(responseBody)
                 Log.d(TAG, result)
                 try {
                     val jsonArray = JSONObject(result).getJSONArray("data")
                     for (i in 0 until jsonArray.length()) {
                         val jsonObject = jsonArray.getJSONObject(i)
-                        val guid = jsonObject.getString("guid")
-                        val position = jsonObject.getString("position")
-                        val company = jsonObject.getString("companyname")
-                        val location = jsonObject.getString("location")
-//                        val notes = jsonObject.getString("notes")
-//                        val thirdparty = jsonObject.getString("thirdparty")
-                        val image = jsonObject.getString("image")
-                        listJob.add("$guid\n $position\n $company\n $location\n")
-
-//                        val originalString = listJob
-//                        val newString = originalString.substring(36)
-
-//                        listGuid.add(guid)
-//                        listJob.add(jsonObject.toString())
+                        listJob.add(jsonObject)
                     }
                     _listJob.value = listJob
                 } catch (e: JSONException) {
@@ -89,7 +66,6 @@ class ListJobViewModel : ViewModel() {
                 responseBody: ByteArray?,
                 error: Throwable?
             ) {
-                // Jika koneksi gagal
                 _isLoading.value = false
                 if (error != null) {
                     _toastMessage.value = when (statusCode) {
@@ -102,11 +78,7 @@ class ListJobViewModel : ViewModel() {
             }
 
         })
-
-
     }
-
-
 
     companion object {
         private const val TAG = "ListJobViewModel"
